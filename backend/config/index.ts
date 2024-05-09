0146b4804d1bebae6e59c60b53ffb72837e0ebb5
HassanKhan8264 <HF610061@GMAIL.COM>
2024-05-09 06:20:55 +0500

    js into ts

--- backend/config/index.ts
+++ backend/config/index.ts
@@ -1,13 +1,12 @@
-// require("dotenv").config();
-// import * as dotenv from "dotenv"
+import * as dotenv from "dotenv";
 
-  
-//   const config = {
-//   PORT: process.env.ENV || 5001,
-//   MONGO: {
-//     URI: process.env.MONGODB_URI,
-//   },
-// };
-  
+dotenv.config();
 
-// module.exports = config;
+const config = {
+  PORT: process.env.ENV || 5001,
+  MONGO: {
+    URI: process.env.MONGODB_URI,
+  },
+};
+
+export default config;
