12a95374655a830c6ccf4f1b8309f7aa9e95cdd8
HassanKhan8264 <HF610061@GMAIL.COM>
2024-05-31 01:10:06 +0500

    compile ts

--- dist/index.js
+++ dist/index.js
@@ -9,12 +9,14 @@ const body_parser_1 = __importDefault(require("body-parser"));
 const cors_1 = __importDefault(require("cors"));
 const routes_1 = __importDefault(require("./routes"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const middleware_1 = __importDefault(require("./middleware"));
 const app = (0, express_1.default)();
 app.use((0, cookie_parser_1.default)());
 app.use(express_1.default.json());
 app.use(body_parser_1.default.json());
 app.use((0, cors_1.default)());
 app.use('/api', routes_1.default);
+app.use(middleware_1.default);
 app.listen(config_1.default.PORT, () => {
     console.log(`Server is running on port ${config_1.default.PORT}`);
 });
