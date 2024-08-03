9dc03f12c07a340c3fef93946b7058b2aba9841c
HassanKhan8264 <HF610061@GMAIL.COM>
2024-08-04 02:50:01 +0500

    working on a counter  app ngrx

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from "@angular/common";
 import { HTTP_INTERCEPTORS } from "@angular/common/http";
 import { AuthInterceptor } from "./interceptor/auth.interceptor";
 import { SharedModule } from "../shared/shared.module";
-
+import { StoreModule, Store } from "@ngrx/store";
+import { counterReducer } from "./store/states/counter/counter.reducer";
 const service = [];
 
 @NgModule({
   declarations: [],
+  imports: [
+    CommonModule,
+    SharedModule,
+    StoreModule.forFeature("counter", counterReducer),
+  ],
   providers: [
     ...service,
     {
@@ -16,13 +22,12 @@ const service = [];
       multi: true,
     },
   ],
-  imports: [CommonModule, SharedModule],
 })
 export class CoreModule {
   constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
     if (parentModule) {
       throw new Error(
-        "CoreModule is already loaded. Import it in the AppModule only",
+        "CoreModule is already loaded. Import it in the AppModule only"
       );
     }
   }
---
+++ client/src/app/core/store/app.states.ts
@@ -0,0 +1,5 @@
+import { CounterState } from "./states/counter/counter.reducer";
+
+export interface AppState {
+  counter: CounterState;
+}
